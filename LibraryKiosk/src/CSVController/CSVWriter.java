3eac70a6dc1d88c51b102351b7b87c7942428137
hyewonee2 <165885976+hyewonee2@users.noreply.github.com>
2024-06-09 11:11:59 +0900

    벡터

--- LibraryKiosk/src/CSVController/CSVWriter.java
+++ LibraryKiosk/src/CSVController/CSVWriter.java
@@ -2,26 +2,34 @@ package CSVController;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Vector;
 
 public class CSVWriter {
 
-    public static void writeUserData(String username, String password, String dob, String phoneNumber, String Name) {
+    public static void writeUserData(Vector<String> userData) {
         String csvFilePath = "userdata.csv";
 
         try (FileWriter writer = new FileWriter(csvFilePath, true)) {
-            writer.append(username)
-                    .append(",")
-                    .append(password)
-                    .append(",")
-                    .append(dob)
-                    .append(",")
-                    .append(phoneNumber)
-                    .append(",")
-                    .append(Name)
-                    .append("\n");
+            StringBuilder sb = new StringBuilder();
+            for (String data : userData) {
+                sb.append(data).append(",");
+            }
+            sb.deleteCharAt(sb.length() - 1); // 마지막 쉼표 제거
+            sb.append("\n");
+            writer.append(sb.toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    public static void main(String[] args) {
+        Vector<String> userData = new Vector<>();
+        userData.add("username");
+        userData.add("password");
+        userData.add("dob");
+        userData.add("phoneNumber");
+        userData.add("Name");
+
+        writeUserData(userData);
+    }
 }
-	
