cc1f7e96686fd415f03c5df8972229e76b6236fc
yezixx <skylow917@daum.net>
2024-06-14 12:48:08 +0900

    화면 전환 코드 수정

--- LibraryKiosk/src/SearchAndReservation/ReserveFinish.java
+++ LibraryKiosk/src/SearchAndReservation/ReserveFinish.java
@@ -93,11 +93,13 @@ public class ReserveFinish extends JPanel {
     // 홈으로
     private void goHome() {
         // 홈 화면으로 이동하는 코드
-        book.setReserved(false);
-        frame.getContentPane().removeAll();
-        MenuFirst menuFirst = new MenuFirst(frame, userinfo);
-        menuFirst.setVisible(true);
-        frame.add(menuFirst);
+        SwingUtilities.invokeLater(() -> {
+            frame.getContentPane().removeAll();
+            MenuFirst menuFirst = new MenuFirst(frame, userinfo);
+            menuFirst.setVisible(true);
+            frame.add(menuFirst);
+            frame.revalidate();
+        });
     }
 
     // 메뉴로
